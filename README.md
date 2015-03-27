8dbdda897f8d50ac5fe3600643f779779145ad04
Prasad Shirsath <prasadss.in@gmail.com>
2015-03-26 19:12:04 -0700

    homescreen+drawer slidding menu added

---
+++ README.md
@@ -0,0 +1,7 @@
+# UDelvr
+Android Application with RESTful services.<br>
+#TechStack<br>
+Spring with JAVA/MongoDB for RESTful Services.<br>
+Android SDK<br>
+Node.js for Server<br>
+Bootstrap/Angular JS for Web Client<br>
---
+++ UDelvr/app/src/main/java/com/udelvr/SplashScreenFragmentSignIn.java
@@ -0,0 +1,43 @@
+
+package com.udelvr;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class SplashScreenFragmentSignIn extends Fragment {
+
+    Button btn_register;
+    ViewGroup root;
+
+	public static Fragment newInstance(Context context) {
+        SplashScreenFragmentSignIn f = new SplashScreenFragmentSignIn();
+
+		return f;
+	}
+
+
+
+    @Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+		 root = (ViewGroup) inflater.inflate(R.layout.activity_spash_screen_signup, container,false);
+
+        btn_register = (Button) root.findViewById(R.id.button_signup);
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), SplashScreenFragmentRegisterNewUser.class);
+                startActivity(intent);
+
+            }
+        });
+
+        return root;
+	}
+	
+}

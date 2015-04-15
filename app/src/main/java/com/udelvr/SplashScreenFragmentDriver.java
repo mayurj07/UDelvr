e1a02968d162fa212eed3ad92761ce58485877dc
Sophia Ngo <sophia.ngo@sjsu.edu>
2015-04-14 19:35:20 -0700

    test

---
+++ app/src/main/java/com/udelvr/SplashScreenFragmentDriver.java
@@ -0,0 +1,26 @@
+
+package com.udelvr;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SplashScreenFragmentDriver extends Fragment {
+
+
+	public static Fragment newInstance(Context context) {
+        SplashScreenFragmentDriver f = new SplashScreenFragmentDriver();
+		
+		return f;
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+		ViewGroup root = (ViewGroup) inflater.inflate(R.layout.activity_spash_screen_driver, null);
+		return root;
+	}
+	
+}
---
+++ app/src/main/java/com/udelvr/SplashScreenFragmentRegisterNewUser.java
@@ -0,0 +1,237 @@
+
+package com.udelvr;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import com.mikhaellopez.circularimageview.CircularImageView;
+import com.udelvr.slidingmenu.MainActivity;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
+import com.facebook.widget.LoginButton.UserInfoChangedCallback;
+
+public class SplashScreenFragmentRegisterNewUser extends Activity {
+
+    private static final String TAG = "Register page";
+    private LoginButton authButton;
+    private UiLifecycleHelper uiHelper;
+    private static final int REQUEST_CAMERA = 100;
+    private static final int SELECT_FILE = 101;
+    ViewGroup root;
+    Button btn_register;
+    CircularImageView circularImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        uiHelper = new UiLifecycleHelper(this, statusCallback);
+        uiHelper.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_details);
+        getActionBar().hide();
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE |
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+        circularImageView = (CircularImageView)this.findViewById(R.id.profilepic);
+        //circularImageView.setBorderColor(getResources().getColor());
+        circularImageView.setBorderWidth(5);
+        circularImageView.addShadow();
+        circularImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+
+        btn_register = (Button)this.findViewById(R.id.button_register);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // facebook login btn
+        authButton = (LoginButton) findViewById(R.id.authButton);
+        authButton.setReadPermissions(Arrays.asList("email","user_location", "user_birthday"));
+        authButton.setUserInfoChangedCallback(new UserInfoChangedCallback() {
+            @Override
+            public void onUserInfoFetched(GraphUser user) {
+                if (user != null) {
+                    Log.e(TAG,"user: " + user + user.getName());
+//                    username.setText("Username: " + user.getName());
+//                    birthday.setText("birthday: " + user.asMap().get("birthday").toString());
+//                    gender.setText("gender: " + user.asMap().get("gender").toString());
+//                    email.setText("email: " + user.asMap().get("email").toString());
+                    //Log.e(TAG,"userid: " +user.getId());
+//                            Log.e(TAG,"email: " + user.asMap());
+                    String imageURL = "https://graph.facebook.com/" + user.getId() + "/picture?type=large";
+                    Log.e(TAG,"image: " + imageURL);
+                    //new LoadProfileImage(profile_pic).execute(imageURL);
+
+                } else {
+                    Log.e(TAG,"You are not log in");
+                }
+            }
+        });
+    }
+
+    private Session.StatusCallback statusCallback = new Session.StatusCallback() {
+        @Override
+        public void call(Session session, SessionState state,
+                         Exception exception) {
+            if (state.isOpened()) {
+                Log.d("MainActivity", "Facebook session opened.");
+            } else if (state.isClosed()) {
+                Log.d("MainActivity", "Facebook session closed.");
+            }
+        }
+    };
+
+
+
+
+
+//	public static Fragment newInstance(Context context) {
+//        SplashScreenFragmentRegisterNewUser f = new SplashScreenFragmentRegisterNewUser();
+//
+//		return f;
+//	}
+
+//    @Override
+//    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+//        super.onActivityCreated(savedInstanceState);
+//
+//       // btn_register = (Button) root.findViewById(R.id.button_signup);
+//
+//
+//    }
+
+//    @Override
+//	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+//		 root = (ViewGroup) inflater.inflate(R.layout.activity_register_details, null);
+//
+//
+//        return root;
+//	}
+
+    private void selectImage() {
+        final CharSequence[] items = { "Take Photo", "Choose from Library",
+                "Cancel" };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add Photo!");
+
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (items[item].equals("Take Photo")) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    File f = new File(android.os.Environment
+                            .getExternalStorageDirectory(), "temp.jpg");
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                    startActivityForResult(intent, REQUEST_CAMERA);
+                } else if (items[item].equals("Choose from Library")) {
+                    Intent intent = new Intent(
+                            Intent.ACTION_PICK,
+                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    intent.setType("image/*");
+                    startActivityForResult(
+                            Intent.createChooser(intent, "Select File"),
+                            SELECT_FILE);
+                } else if (items[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_CAMERA) {
+                File f = new File(Environment.getExternalStorageDirectory()
+                        .toString());
+                for (File temp : f.listFiles()) {
+                    if (temp.getName().equals("temp.jpg")) {
+                        f = temp;
+                        break;
+                    }
+                }
+                try {
+                    Bitmap bm;
+                    BitmapFactory.Options btmapOptions = new BitmapFactory.Options();
+
+                    bm = BitmapFactory.decodeFile(f.getAbsolutePath(),
+                            btmapOptions);
+
+                    // bm = Bitmap.createScaledBitmap(bm, 70, 70, true);
+                    circularImageView.setImageBitmap(bm);
+
+                    String path = android.os.Environment
+                            .getExternalStorageDirectory()
+                            + File.separator
+                            + "Phoenix" + File.separator + "default";
+                    f.delete();
+                    OutputStream fOut = null;
+                    File file = new File(path, String.valueOf(System
+                            .currentTimeMillis()) + ".jpg");
+                    try {
+                        fOut = new FileOutputStream(file);
+                        bm.compress(Bitmap.CompressFormat.JPEG, 85, fOut);
+                        fOut.flush();
+                        fOut.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (requestCode == SELECT_FILE) {
+                Uri selectedImageUri = data.getData();
+
+                String tempPath = getPath(selectedImageUri, this);
+                Bitmap bm;
+                BitmapFactory.Options btmapOptions = new BitmapFactory.Options();
+                bm = BitmapFactory.decodeFile(tempPath, btmapOptions);
+                circularImageView.setImageBitmap(bm);
+            }
+        }
+    }
+    public String getPath(Uri uri, Activity activity) {
+        String[] projection = { MediaStore.MediaColumns.DATA };
+        Cursor cursor = activity
+                .managedQuery(uri, projection, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+    }
+}
